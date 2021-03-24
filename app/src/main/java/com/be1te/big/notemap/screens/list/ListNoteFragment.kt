468d7cc001058760aae31d02e65eb7a19bb47c81
BeL1te <aluzi1999@gmail.com>
2021-03-24 19:23:36 +0300

    Added navigation_graph for MainActivity. Added 4 fragments.

---
+++ app/src/main/java/com/be1te/big/notemap/screens/list/ListNoteFragment.kt
@@ -0,0 +1,19 @@
+package com.be1te.big.notemap.screens.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.be1te.big.notemap.R
+
+class ListNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_note, container, false)
+    }
+}
